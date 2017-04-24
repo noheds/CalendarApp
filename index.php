8b33801e10a046bab916aadf91ebbbd745ed87f5
Nohelya Dos Santos <noheadr@gmail.com>
2017-04-23 21:54:26 -0500

    nueva app de herou, ya incluido el campo fecha y actualizadas las credenciales de acceso db

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli('us-cdbr-east.cleardb.com', 'adffdadf2341', 'adf4234', 'heroku_db');
+$conn = new mysqli('us-cdbr-iron-east-03.cleardb.net', 'abfd7d70e584dce', 'b6eb7512', 'heroku_a41f85eea948e5b');
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -11,11 +11,12 @@ $action=$_POST['action'];
 $name=$_POST['Evento'];
 $place=$_POST['Lugar'];
 $time=$_POST['Hora'];
+$date=$_POST['Fecha'];
 
 
 if($action=='insert'){
 
-	$sql = "INSERT INTO eventos (Evento,Lugar,Hora) VALUES ('$name','$place',$time')";
+	$sql = "INSERT INTO eventos (Evento,Lugar,Hora,Fecha) VALUES ('$name','$place',$time','$date')";
 
 	if ($conn->query($sql) === TRUE) {
 		echo "New record created successfully";
